92095c53a2fc0b8f472409b276ef39cb084015e7
haritha Chandana <harithachandana10@gmail.com>
2024-07-23 11:26:01 +0530

    TestCommit

---
+++ src/ConnectionImages.js
@@ -0,0 +1,15 @@
+import Cart from "./Cart.js";
+function Connections(){
+const arr = ['a','b','c','d','e','f','g','h'];
+return <div style={{display:"flex",flexFlow:"row wrap"}}>
+                <Cart path={'boy-1.jpg'} name={'Scott Ayres'} description={'Resource Person'}/>
+                <Cart path={'girl-1.jpg'} name={'Samantha'} description={'Resource Person'}/>
+                <Cart path={'boy-2.png'} name={'Scott Ayres'} description={'Resource Person'}/>
+                <Cart path={'girl-1.jpg'} name={'Scott Ayres'} description={'Resource Person'}/>
+                <Cart path={'girl-2.jpg'} name={'Samantha'} description={'Resource Person'}/>
+                <Cart path={'girl-3.jpg'} name={'Scott Ayres'} description={'Resource Person'}/>
+                <Cart path={'boy-2.png'} name={'Scott Ayres'} description={'Resource Person'}/>
+                <Cart path={'girl-1.jpg'} name={'Scott Ayres'} description={'Resource Person'}/>
+         </div>
+}
+export default Connections;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+// import Rev from "./Reviews-Descriptions.js";
+import Connections from "./ConnectionImages.js";
+var rootElement = document.getElementById("root");
+var rootReference = ReactDOM.createRoot(rootElement);
+// rootReference.render(<Rev/>);
+rootReference.render(<Connections/>);
