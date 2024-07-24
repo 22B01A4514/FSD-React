f48e5e3dfafcf4c2b52192525bfafc3b9e5b68e5
haritha Chandana <harithachandana10@gmail.com>
2024-07-24 13:13:41 +0530

    TestCommit1

--- src/Cart.js
+++ src/Cart.js
@@ -2,7 +2,7 @@ function Cart({path,name,description}){
     return <div style={{padding:50,backgroundColor:"white",margin:7,borderRadius:10,boxShadow:"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
         <center>
                 <img src={path} width={100} height={100} style={{borderRadius:200}}></img>
-                <h1 >{name} <span style={{fontWeight:"normal",fontSize:15}}>.2nd</span></h1>
+                <h1 >{name} <span style={{fontWeight:"normal ",fontSize:15}}>.2nd</span></h1>
                 <p>{description}</p>
         </center>
                 
